3dd373590c7e3c1a70e3f03b3c36d6387407b8b3
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-27 00:20:23 +0530

    Welcome message

---
+++ src/com/bridgeLabz/HotelReservationSystem.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("**********************************WELCOME IN HOTEL RESERVATION SYSTEM**************************************");
+    }
+}
