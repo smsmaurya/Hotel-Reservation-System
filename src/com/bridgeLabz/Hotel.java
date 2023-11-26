352ff2094014dc5d6a65b8f2e9a311a4223a3c87
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-27 01:20:33 +0530

    [Added] : Ability to find cheapest hotel from hotel reservation system

--- src/com/bridgeLabz/Hotel.java
+++ src/com/bridgeLabz/Hotel.java
@@ -1,20 +1,40 @@
 package com.bridgeLabz;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Hotel {
     String hotelName;
-    int weekdayRegularCustomer;
+    int weekdayRateForRegularCustomer,weekendRateForRegularCustomer;
 
-    public Hotel(String hotelName, int regularCustomer) {
+    public Hotel(String hotelName, int weekdayRateForRegularCustomer,int weekendRateForRegularCustomer) {
         this.hotelName = hotelName;
-        this.weekdayRegularCustomer = regularCustomer;
+        this.weekdayRateForRegularCustomer = weekdayRateForRegularCustomer;
+        this.weekendRateForRegularCustomer = weekendRateForRegularCustomer;
     }
 
     @Override
     public String toString() {
         return "Hotel{" +
                 "hotelName='" + hotelName + '\'' +
-                ", regularCustomer=" + weekdayRegularCustomer +
+                ", weekdayRegularCustomer=" + weekdayRateForRegularCustomer + '\'' +
+                ", weekendRegularCustomer=" + weekendRateForRegularCustomer +
                 '}';
+    }
+
+    public int calculateTotalRate(LocalDate startDate, LocalDate endDate) {
+        int totalRate = 0;
+
+        while (!startDate.isAfter(endDate)) {
+            if (startDate.getDayOfWeek() == DayOfWeek.SATURDAY || startDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                totalRate += weekendRateForRegularCustomer;
+            } else {
+                totalRate += weekdayRateForRegularCustomer;
+            }
+            startDate = startDate.plusDays(1);
+        }
+
+        return totalRate;
     }
 
 }
--- src/com/bridgeLabz/HotelReservationSystem.java
+++ src/com/bridgeLabz/HotelReservationSystem.java
@@ -1,18 +1,39 @@
 package com.bridgeLabz;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class HotelReservationSystem {
     ArrayList<Hotel> hotelList=new ArrayList<>();
+    public void addHotel(Hotel hotel) {hotelList.add(hotel);}
+
+    public Hotel findCheapestHotel(LocalDate startDate, LocalDate endDate) {
+        return hotelList.stream()
+                .min(Comparator.comparingInt(hotel -> hotel.calculateTotalRate(startDate, endDate)))
+                .orElse(null);
+    }
+
     public static void main(String[] args) {
         System.out.println("**********************************WELCOME IN HOTEL RESERVATION SYSTEM**************************************\n");
-        Hotel Lakewood = new Hotel( "Lakewood", 110);
-        Hotel Bridgewood = new Hotel( "Bridgewood", 160);
-        Hotel Ridgewood = new Hotel("Ridgewood", 220);
+
         HotelReservationSystem hrs = new HotelReservationSystem();
-        hrs.hotelList.add(Lakewood);
-        hrs.hotelList.add(Bridgewood);
-        hrs.hotelList.add(Ridgewood);
-        hrs.hotelList.forEach(System.out::println);
+
+        Hotel lakewood = new Hotel("Lakewood", 110, 90);
+        Hotel bridgewood = new Hotel("Bridgewood", 160, 60);
+        Hotel ridgewood = new Hotel("Ridgewood", 220, 150);
+
+        hrs.addHotel(lakewood);
+        hrs.addHotel(bridgewood);
+        hrs.addHotel(ridgewood);
+
+        LocalDate startDate = LocalDate.parse("2023-11-23", DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate endDate = LocalDate.parse("2023-11-26", DateTimeFormatter.ISO_LOCAL_DATE);
+
+        Hotel cheapestHotel = hrs.findCheapestHotel(startDate, endDate);
+        int totalRate = cheapestHotel.calculateTotalRate(startDate, endDate);
+
+        System.out.println("Cheapest Hotel: " + cheapestHotel.hotelName + ", Total Rates: $" + totalRate);
     }
 }
