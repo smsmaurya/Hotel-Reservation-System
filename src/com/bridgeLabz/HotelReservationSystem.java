aebf2ec2a4776a27db8c4d1f2db08b590ea79cf7
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-27 17:21:22 +0530

    [Added] : Ability to add weekend and weekday rates for each hotel

--- src/com/bridgeLabz/HotelReservationSystem.java
+++ src/com/bridgeLabz/HotelReservationSystem.java
@@ -7,7 +7,9 @@ import java.util.Comparator;
 
 public class HotelReservationSystem {
     ArrayList<Hotel> hotelList=new ArrayList<>();
-    public void addHotel(Hotel hotel) {hotelList.add(hotel);}
+    public void addHotel(Hotel hotel) {
+        hotelList.add(hotel);
+    }
 
     public Hotel findCheapestHotel(LocalDate startDate, LocalDate endDate) {
         return hotelList.stream()
@@ -21,15 +23,15 @@ public class HotelReservationSystem {
         HotelReservationSystem hrs = new HotelReservationSystem();
 
         Hotel lakewood = new Hotel("Lakewood", 110, 90);
-        Hotel bridgewood = new Hotel("Bridgewood", 160, 60);
+        Hotel bridgewood = new Hotel("Bridgewood", 150, 50);
         Hotel ridgewood = new Hotel("Ridgewood", 220, 150);
 
         hrs.addHotel(lakewood);
         hrs.addHotel(bridgewood);
         hrs.addHotel(ridgewood);
 
-        LocalDate startDate = LocalDate.parse("2023-11-23", DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDate endDate = LocalDate.parse("2023-11-26", DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate startDate = LocalDate.parse("2023-11-25", DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate endDate = LocalDate.parse("2023-11-27", DateTimeFormatter.ISO_LOCAL_DATE);
 
         Hotel cheapestHotel = hrs.findCheapestHotel(startDate, endDate);
         int totalRate = cheapestHotel.calculateTotalRate(startDate, endDate);
